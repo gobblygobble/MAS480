9826904690238892a7371d13078991cacc72ae44
gobblygobble <gobblygobble@kaist.ac.kr>
2020-07-07 01:04:22 +0900

    loss data erased - only csv files left

--- loss/find-loss.py
+++ loss/find-loss.py
@@ -1,19 +1,31 @@
-import os
-'''
-# open all files in current directory
-for filename in os.listdir(os.getcwd()):
-    with open(os.path.join(os.getcwd(), filename), 'r') as f:
-        # work with files that end with '.txt'
-        if not filename.endswith(".txt"):
-            continue
-        # note that f.name will return filename!
-        print("NEW FILE: {}".format(filename))
-        for line in f:
-            print(line.rstrip())
-'''
-with open("csv-unet-without-res-64.csv", "a+") as wf:
-    with open("unet-64-without-res-loss.txt", 'r') as rf:
+#FILL IN MY FILE NAME HERE
+data_type = "att-unet-128"
+#data_type = "noatt-unet-320"
+
+rawdata_filename = data_type + ".txt"
+trainloss_filename = "trainloss-" + data_type + ".csv"
+validationloss_filename = "validationloss-" + data_type + ".csv"
+validationacc_filename = "validationaccuracy-" + data_type + ".csv"
+
+with open(trainloss_filename, "a+") as wf1:
+    with open(rawdata_filename, 'r') as rf:
         for line in rf:
             line = line.strip()
             if line.startswith("loss: ") and line.endswith("500)"):
-                wf.write(line.split()[1][0:7] + ",")
+                wf1.write(line.split()[1][0:7] + ",")
+'''
+with open(validationloss_filename, "a+") as wf2:
+    with open(rawdata_filename, 'r') as rf:
+        for line in rf:
+            line = line.strip()
+            if line.startswith("validation loss:"):
+                wf2.write(line.split()[2][0:7] + ",")
+'''
+with open(validationacc_filename, "a+") as wf3:
+    with open(rawdata_filename, 'r') as rf:
+        for line in rf:
+            line = line.strip()
+            if line.startswith("validation average IoU:"):
+                wf3.write(line.split()[3][0:7] + ",")
+
+
